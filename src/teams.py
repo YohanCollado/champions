40e1efe1282abd6b5443a70ecd4fd328a21cec69
YohanCollado <yohancollado@yahoo.com>
2026-06-26 20:08:58 -0400

    Teams added

---
+++ src/teams.py
@@ -0,0 +1,19 @@
+# 42 Champions Path teams
+CHAMPIONS_PATH_TEAMS = []
+
+# 10 League Path teams
+LEAGUE_PATH_TEAMS = []
+
+# 29 Qualified team
+DIRECT_QUALIFIED_TEAMS = [
+    "Arsenal", "Aston Villa", "Atletico Madrid",
+    "Borussia Dortmund", "Bayern Munchen", "Barcelona",
+    "Club Brugge", "Como", "Feyenoord",
+    "Galatasaray", "Inter", "Leipzig",
+    "Lens", "Lile", "Liverpool",
+    "Manchester City", "Manchester United", "Napoli",
+    "Paris Saint Germain", "Porto", "PSV",
+    "Real Betis", "Real Madrid", "Roma",
+    "Shakhtar", "Slavia Praha", "Sporting CP", 
+    "Stuttgart", "Villareal"
+    ]
